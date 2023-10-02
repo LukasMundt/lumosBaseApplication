94185b263ac62f96fa4b44419910e8356ff9ca51
Lukas Mundt <lukas@lukas-mundt.de>
2023-10-02 21:50:04 +0200

    Testzwecke

--- config/app.php
+++ config/app.php
@@ -160,6 +160,7 @@ return [
          * Package Service Providers...
          */
         Spatie\Permission\PermissionServiceProvider::class,
+        Lukasmundt\Akquise\Providers\AkquiseProvider::class,
 
         /*
          * Application Service Providers...
