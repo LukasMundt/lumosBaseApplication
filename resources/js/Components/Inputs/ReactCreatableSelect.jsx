1427660885a20ed66a5a7e73efa4e8d666fdf009
Lukas Mundt <lukas@lukas-mundt.de>
2023-11-11 13:58:57 +0100

    Darstellung von reactSelectComponents angepasst

--- resources/js/Components/Inputs/ReactCreatableSelect.jsx
+++ resources/js/Components/Inputs/ReactCreatableSelect.jsx
@@ -32,6 +32,7 @@ export default function ReactCreatableSelect({
                 option: (state) => "dark:text-gray-300",
                 menu: (state) => "dark:bg-gray-800",
                 multiValueRemove: (state) => "bg-red-300 text-gray-700",
+                singleValue: (state) => "dark:text-gray-200 text-gray-800"
             }}
             theme={(theme) => ({
                 ...theme,
--- resources/js/Components/Inputs/ReactSelect.jsx
+++ resources/js/Components/Inputs/ReactSelect.jsx
@@ -1,4 +1,6 @@
+import { usePage } from "@inertiajs/react";
 import Select from "react-select";
+// import { StylesConfig } from 'react-select';
 
 export default function ReactSelect({
     isMulti = false,
@@ -18,19 +20,15 @@ export default function ReactSelect({
             isClearable={isClearable}
             options={options}
             onChange={onChange}
-            // styles={{
-            //     control: (baseStyles, state) => ({
-            //         ...baseStyles,
-            //         borderColor: state.isFocused ? 'grey' : 'red',
-            //         backgroundColor: '#141826',
-            //       }),
-            // }}
+            
             classNames={{
                 control: (state) =>
                     "border-gray-300 dark:bg-gray-900 dark:border-gray-700 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm",
                 option: (state) => "dark:text-gray-300",
                 menu: (state) => "dark:bg-gray-800",
+                menuPortal: (state) => "bg-red-800",
                 multiValueRemove: (state) => "bg-red-300 text-gray-700",
+                singleValue: (state) => "dark:text-gray-200 text-gray-800"
             }}
             theme={(theme) => ({
                 ...theme,
@@ -42,7 +40,6 @@ export default function ReactSelect({
                 },
             })}
             {...props}
-            // className={"  "+className}
         />
     );
 }
