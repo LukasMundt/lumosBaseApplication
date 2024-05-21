8ce1d5b51f0644db836f9373dde881a806895ace
Lukas Mundt <lukas@lukas-mundt.de>
2024-05-21 11:26:51 +0200

    fix: build not possible

--- vite.config.js
+++ vite.config.js
@@ -9,65 +9,72 @@ import manifestSRI from "vite-plugin-manifest-sri";
 export default defineConfig({
     build: {
         minify: "terser",
-        sourcemap: true
+        sourcemap: true,
     },
-    plugins: [[
-        viteCompression({
-            deleteOriginFile: false,
-            ext: "",
+    plugins: [
+        [
+            viteCompression({
+                deleteOriginFile: false,
+                ext: "",
+            }),
+        ],
+        VitePWA({
+            registerType: "autoUpdate",
+            injectRegister: "auto",
+            manifest: {
+                name: "Lumos",
+                short_name: "Lumos",
+                theme_color: "#ffffff",
+                lang: "de",
+                orientation: "portrait",
+                scope: "/",
+                start_url: "/",
+                icons: [
+                    {
+                        src: "/logo-400x400.png",
+                        sizes: "400x400",
+                        type: "image/png",
+                        // purpose: "maskable",
+                    },
+                    {
+                        src: "/logo-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                    {
+                        src: "/logo-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+                screenshots: [
+                    {
+                        src: "/screenshot1.png",
+                        sizes: "1918x937",
+                        type: "image/png",
+                        form_factor: "wide",
+                        label: "The Login Page",
+                    },
+                    {
+                        src: "/screenshot2.jpg",
+                        sizes: "1080x2031",
+                        type: "image/jpeg",
+                        form_factor: "narrow",
+                        label: "The login page on mobile screens",
+                    },
+                ],
+            },
         }),
-    ], VitePWA({
-        registerType: "autoUpdate",
-        injectRegister: "auto",
-        manifest: {
-            name: "Lumos",
-            short_name: "Lumos",
-            theme_color: "#ffffff",
-            lang: "de",
-            orientation: "portrait",
-            scope: "/",
-            start_url: "/",
-            icons: [
-                {
-                    src: "/logo-400x400.png",
-                    sizes: "400x400",
-                    type: "image/png",
-                    // purpose: "maskable",
-                },
-                {
-                    src: "/logo-512x512.png",
-                    sizes: "512x512",
-                    type: "image/png",
-                    purpose: "maskable",
-                },
-                {
-                    src: "/logo-512x512.png",
-                    sizes: "512x512",
-                    type: "image/png",
-                },
-            ],
-            screenshots: [
-                {
-                    src: "/screenshot1.png",
-                    sizes: "1918x937",
-                    type: "image/png",
-                    form_factor: "wide",
-                    label: "The Login Page",
-                },
-                {
-                    src: "/screenshot2.jpg",
-                    sizes: "1080x2031",
-                    type: "image/jpeg",
-                    form_factor: "narrow",
-                    label: "The login page on mobile screens",
-                },
-            ],
-        },
-    }), laravel({
-        input: "resources/js/app.jsx",
-        refresh: true,
-    }), react(), manifestSRI(), sentryVitePlugin({
-        org: import.meta.env.VITE_SENTRY_ORG,
-        project: import.meta.env.VITE_SENTRY_PROJECT
-    })],
+        laravel({
+            input: "resources/js/app.jsx",
+            refresh: true,
+        }),
+        react(),
+        manifestSRI(),
+        // sentryVitePlugin({
+        //     project: import.meta.env.VITE_SENTRY_PROJECT,
+        //     org: import.meta.env.VITE_SENTRY_ORG,
+        // }),
+    ],
 });
