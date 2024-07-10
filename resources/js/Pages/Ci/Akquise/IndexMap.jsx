cb6bb7dfe617a32500c4b937501586272409b373
Lukas Mundt <lukas@lukas-mundt.de>
2024-07-10 09:28:22 +0200

    fix: map is more stable now

--- resources/js/Pages/Ci/Akquise/IndexMap.jsx
+++ resources/js/Pages/Ci/Akquise/IndexMap.jsx
@@ -1,160 +1,73 @@
-import { Head, router, usePage, useRemember } from "@inertiajs/react";
+import { Head, usePage, useRemember } from "@inertiajs/react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import MyMapMulti from "./partials/MyMapMulti";
 import { useGeolocation } from "@uidotdev/usehooks";
 import { Switch } from "@/Components/ui/switch";
 import { Label } from "@/Components/ui/label";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { CreatePopup } from "./partials/CreatePopup";
-import { Tabs, TabsList, TabsTrigger } from "@/Components/ui/tabs";
-import { Input } from "@/Components/ui/input";
-import { Button } from "@/Components/ui/button";
-import { Search } from "lucide-react";
 
-export default function IndexMap({ domain }) {
-    const { user, auth, markers, centerAdr } = usePage().props;
-    const location = useGeolocation();
-    const [center, setCenter] = useState({
-        latitude: 53.55522722948935,
-        longitude: 9.995317259820599,
-    });
-    const [creatorMode, setCreatorMode] = useRemember(false);
-    const [selectedLocation, selectLocation] = useState(null);
-    const [search, setSearch] = useState("");
-    const [centerChanged, setCenterChanged] = useState(false);
+export default function IndexMap({}) {
+  const { user, auth, markers } = usePage().props;
+  const location = useGeolocation();
+  const [creatorMode, setCreatorMode] = useRemember(false);
+  const [selectedLocation, selectLocation] = useState(null);
 
-    useEffect(() => {
-        if (search == "") {
-            const newCenter =
-                location.loading || location.error
-                    ? {
-                          latitude: 53.55522722948935,
-                          longitude: 9.995317259820599,
-                      }
-                    : {
-                          latitude: location.latitude,
-                          longitude: location.longitude,
-                      };
-            if (newCenter != center) {
-                setCenterChanged(true);
+  const handleCoordinatesClick = (lat, lng) => {
+    // console.log(lat);
+    selectLocation({ lat: lat, lon: lng });
+  };
+
+  const unselectLocation = () => {
+    selectLocation(null);
+  };
+
+  return (
+    <AuthenticatedLayout
+      user={auth.user}
+      header={
+        <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+          Übersicht
+        </h2>
+      }
+    >
+      <Head title="Übersicht" />
+
+      <div
+        className="h-full flex flex-col"
+        // style={{ height: bodyHeight - (headerHeight + navHeight + 1) + "px" }}
+      >
+        <div className="w-full flex justify-end items-center mb-2">
+          <Switch
+            id="creator-mode"
+            checked={creatorMode}
+            aria-label="toggle creator mode"
+            onCheckedChange={(e) => setCreatorMode(!creatorMode)}
+          ></Switch>
+          <Label htmlFor="creator-mode" className="ml-2">
+            Creator Mode
+          </Label>
+          <CreatePopup
+            openParam={selectedLocation != null}
+            unselectLocation={unselectLocation}
+            location={selectedLocation}
+          />
+        </div>
+        <div className="mx-auto space-y-6 w-full h-full">
+          <MyMapMulti
+            center={
+              location.loading || location.error
+                ? [53.55522722948935, 9.995317259820599]
+                : [location.latitude, location.longitude]
             }
-            setCenter(newCenter);
-        }
-        // else if (center != centerAdr) {
-        //     setCenter(centerAdr);
-        //     setCenterChanged(true);
-        // }
-    }, [location, search, centerAdr]);
-
-    const handleCoordinatesClick = (lat, lng) => {
-        selectLocation({ lat: lat, lon: lng });
-    };
-
-    const unselectLocation = () => {
-        selectLocation(null);
-    };
-
-    function handleSearch() {
-        router.visit(
-            route("akquise.akquise.map", {
-                domain: domain,
-                search: search,
-            }),
-            { preserveState: true }
-        );
-    }
-
-    return (
-        <AuthenticatedLayout
-            user={auth.user}
-            header={
-                <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                    Übersicht
-                </h2>
-            }
-        >
-            <Head title="Übersicht" />
-
-            <div
-                className="h-full flex flex-col"
-                // style={{ height: bodyHeight - (headerHeight + navHeight + 1) + "px" }}
-            >
-                <div className="w-full flex justify-between items-center mb-2">
-                    <div className="flex gap-3">
-                        <Tabs
-                            defaultValue="map"
-                            className="w-[200px]"
-                            onValueChange={(value) => {
-                                if (value === "table") {
-                                    router.visit(
-                                        route("akquise.akquise.index", {
-                                            domain: domain,
-                                        })
-                                    );
-                                }
-                            }}
-                        >
-                            <TabsList className="grid w-full grid-cols-2">
-                                <TabsTrigger value="table">Tabelle</TabsTrigger>
-                                <TabsTrigger value="map">Karte</TabsTrigger>
-                            </TabsList>
-                        </Tabs>
-                        {/* <Input
-                            id="search"
-                            onKeyDown={(e) => {
-                                if (e.code === "Enter") {
-                                    handleSearch();
-                                }
-                            }}
-                            onChange={(e) => setSearch(e.target.value)}
-                        />
-                        <Button
-                            size="icon"
-                            type="button"
-                            onClick={handleSearch}
-                            title="Search"
-                        >
-                            <Search width={40}/>
-                        </Button> */}
-                    </div>
-                    <div>
-                        <Switch
-                            id="creator-mode"
-                            checked={creatorMode}
-                            aria-label="toggle creator mode"
-                            onCheckedChange={(e) =>
-                                setCreatorMode(!creatorMode)
-                            }
-                        ></Switch>
-                        <Label htmlFor="creator-mode" className="ml-2">
-                            Creator Mode
-                        </Label>
-                    </div>
-
-                    <CreatePopup
-                        openParam={selectedLocation != null}
-                        unselectLocation={unselectLocation}
-                        location={selectedLocation}
-                    />
-                </div>
-                <div className="mx-auto space-y-6 w-full h-full">
-                    <MyMapMulti
-                        // center={[center.latitude, center.longitude]}
-                        center={
-                            location.loading || location.error
-                                ? [53.55522722948935, 9.995317259820599]
-                                : [location.latitude, location.longitude]
-                        }
-                        markers={markers}
-                        zoom={18}
-                        height={null}
-                        getCoordinates={
-                            creatorMode ? handleCoordinatesClick : null
-                        }
-                        locationLoaded={!location.loading && !location.error}
-                    />
-                </div>
-            </div>
-        </AuthenticatedLayout>
-    );
+            markers={markers}
+            zoom={18}
+            height={null}
+            getCoordinates={creatorMode ? handleCoordinatesClick : null}
+            locationLoaded={!location.loading && !location.error}
+          />
+        </div>
+      </div>
+    </AuthenticatedLayout>
+  );
 }
